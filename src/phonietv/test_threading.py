47784854e64571be0ad8e5ea93e9415009311066
Francis Conroy <francisc@posteo.net>
2026-07-31 11:40:14 +1000

    Add a basic test for the threaded tasks

--- src/phonietv/test_threading.py
+++ src/phonietv/test_threading.py
@@ -1,3 +1,4 @@
+import logging
 import queue
 import threading
 import time
@@ -6,32 +7,53 @@ from unittest import TestCase
 from .event import PhonieTVEvent
 from .threading import PhonieTVTask
 
-class PhonieTVTaskTester(PhonieTVTask):
+logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(message)s")
+LOGGER = logging.getLogger(__name__)
+
+class PhoneTVTaskTesterA(PhonieTVTask):
 
     def task_function(self, stop_event: threading.Event):
         while not stop_event.is_set():
             try:
                 event_to_process = self.inbound_queue.get_nowait()
-                print(f"got event {event_to_process.eventType}")
-                self.publish_event(PhonieTVEvent("response_event", {"response": "ok"}))
+                LOGGER.info(f"got event {event_to_process.eventType}")
+                if event_to_process.eventType == "final_event":
+                    LOGGER.info(f"Stopping thread {self.task_name}")
+                    break
+                else:
+                    self.publish_event(PhonieTVEvent("response_event", {"response": "ok"}))
 
             except queue.Empty:
                 pass
             time.sleep(1)
-            print(f"Thread {self.task_name} is ticking")
+            LOGGER.info(f"Thread {self.task_name} is ticking")
+
+
+class PhonieTVTaskTesterB(PhonieTVTask):
+
+    def task_function(self, stop_event: threading.Event):
+        while not stop_event.is_set():
+            event_to_process = self.inbound_queue.get(block=True)
+            LOGGER.info(f"got event {event_to_process.eventType}")
+            if event_to_process.eventType == "response_event":
+                self.publish_event(PhonieTVEvent("final_event", {"response": "ok"}))
+                LOGGER.info(f"Stopping thread {self.task_name}")
+                break
 
 
 class TestPhonieTVTask(TestCase):
     def test_task_function(self):
         stop_event = threading.Event()
-        task = PhonieTVTaskTester("test task", stop_event)
-        other_queue = queue.Queue()
-        task.attach_event_queues([other_queue])
-        task.start()
+        taska = PhoneTVTaskTesterA("test task A", stop_event)
+        taskb = PhonieTVTaskTesterB("test task B", stop_event)
+        # other_queue = queue.Queue()
+        taska.attach_event_queues({taskb.inbound_queue})
+        taskb.attach_event_queues({taska.inbound_queue})
+        taska.start()
+        taskb.start()
         time.sleep(2)
-        task.inbound_queue.put(PhonieTVEvent("party_event", {}))
-        time.sleep(1)
-        print(other_queue.get())
-        stop_event.set()
-        task.join()
-
+        taska.inbound_queue.put(PhonieTVEvent("party_event", {}))
+        # time.sleep(1)
+        # stop_event.set()
+        taska.join()
+        taskb.join()
