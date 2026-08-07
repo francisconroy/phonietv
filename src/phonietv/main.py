4cbdc1f9320d1a04dbb9069ab094afda68274fa4
Francis Conroy <francisc@posteo.net>
2026-08-07 14:07:15 +1000

    Make main work

--- src/phonietv/main.py
+++ src/phonietv/main.py
@@ -7,8 +7,6 @@ def main():
     ## This app uses Queues to communicate events
 
     # Set up the modules and connect them together as needed
-    timer =
-
 
 if __name__ == "__main__":
     main()
