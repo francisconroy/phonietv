8f7b5ca70cc76843a1be51b8b5b443ad6119e1f3
Francis Conroy <francisc@posteo.net>
2026-08-12 21:41:33 +1000

    INfor level when publishing events

--- src/phonietv/threading.py
+++ src/phonietv/threading.py
@@ -40,7 +40,7 @@ class PhonieTVTask(ABC):
         :param event:
         :return:
         """
-        LOGGER.warning(f"{self.task_name}: publishing event {event.event_type}")
+        LOGGER.info(f"{self.task_name}: publishing event {event.event_type}, payload: {event.event_payload}")
         with self.event_queue_lock:
             for queue in self.event_queues:
                 queue.put(event)
